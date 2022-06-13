0cbc48114cab06842411a70c00f4c6bd2446bd7b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-06-13 23:39:46 +0200

    Add plugin

---
+++ index.js
@@ -0,0 +1,15 @@
+import { plugin } from "tailwindcss/plugin";
+
+module.exports = plugin(function ({ addVariant, e, postcss }) {
+ addVariant("firefox", ({ container, separator }) => {
+  const isFirefox = postcss.atRule({
+   name: "-moz-document",
+   params: "url-prefix()",
+  });
+  isFirefox.append(container.nodes);
+  container.append(isFirefox);
+  isFirefox.walkRules((rule) => {
+   rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`;
+  });
+ });
+})

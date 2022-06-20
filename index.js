369a016891230d9601410c5a843db3e6a89b3ece
Igor Kowalczyk <majonezexe@protonmail.com>
2022-06-20 19:02:57 +0200

    Update code

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const plugin = require("tailwindcss/plugin");
 
-module.exports = plugin(function ({ addVariant, e, postcss }) {
- addVariant("firefox", ({ container, separator }) => {
-  const isFirefox = postcss.atRule({
-   name: "-moz-document",
-   params: "url-prefix()",
-  });
-  isFirefox.append(container.nodes);
-  container.append(isFirefox);
-  isFirefox.walkRules((rule) => {
-   rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`;
-  });
- });
+module.exports = plugin(function({ addVariant }) {
+ addVariant('firefox', '@-moz-document url-prefix()')
 });

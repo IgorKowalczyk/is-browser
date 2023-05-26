c335bfbc155e3866d40a6b9ec169c36186cc4ea1
Igor Kowalczyk <majonezexe@protonmail.com>
2023-05-26 23:49:17 +0200

    Fix build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import plugin from "tailwindcss/plugin";
 
-const isBrowser = plugin(({ addVariant }) => {
+/* eslint-disable func-names,prefer-arrow-callback,space-before-function-paren */
+const isBrowser = plugin(function ({ addVariant }) {
  addVariant("firefox", "@supports (-moz-appearance: none)");
  addVariant("safari", "@supports (background: -webkit-named-image(i))");
  addVariant("chrome", "@supports (-webkit-app-region: inherit)");

124eb25499acd2640100f19187602c2b57bef6b6
Igor Kowalczyk <majonezexe@protonmail.com>
2025-04-27 18:33:23 +0200

    Update packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,10 @@
 import eslintConfig from "@igorkowalczyk/eslint-config";
+import { defineConfig } from "eslint/config";
 
-export default [
+export default defineConfig([
  // prettier
- ...eslintConfig.base,
- ...eslintConfig.node,
- ...eslintConfig.typescript,
- ...eslintConfig.prettier,
-];
+ eslintConfig.base,
+ eslintConfig.node,
+ eslintConfig.typescript,
+ eslintConfig.prettier,
+]);
